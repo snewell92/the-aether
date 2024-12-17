5f64ca1bbf8b34a13e024b55091f24b866d94c40
bconlon <bconlon.gml@gmail.com>
2024-12-16 18:45:36 -0800

    fix: infinity will work if applied to dart shooter in creative

--- src/main/java/com/aetherteam/aether/item/combat/DartItem.java
+++ src/main/java/com/aetherteam/aether/item/combat/DartItem.java
@@ -2,14 +2,15 @@ package com.aetherteam.aether.item.combat;
 
 import com.aetherteam.aether.entity.projectile.dart.AbstractDart;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ArrowItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.ProjectileItem;
+import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraft.world.level.Level;
 
 import javax.annotation.Nullable;
 
-public abstract class DartItem extends Item implements ProjectileItem {
+public abstract class DartItem extends ArrowItem implements ProjectileItem {
     public DartItem(Properties properties) {
         super(properties);
     }
@@ -22,4 +23,9 @@ public abstract class DartItem extends Item implements ProjectileItem {
      * @return {@link AbstractDart} entity created from dart entity type.
      */
     public abstract AbstractDart createDart(Level level, ItemStack ammo, LivingEntity shooter, @Nullable ItemStack firedFromWeapon);
+
+    @Override
+    public boolean isInfinite(ItemStack ammo, ItemStack weapon, LivingEntity livingEntity) {
+        return weapon.getEnchantmentLevel(livingEntity.level().holderOrThrow(Enchantments.INFINITY)) > 0;
+    }
 }
